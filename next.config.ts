f721c6c1d1e16be326e9f07e2193a8fe4e120a1e
abhinav20git <118235094+abhinav20git@users.noreply.github.com>
2025-03-31 09:38:03 +0530

    ui updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+    });
+  
+    config.plugins.push(new MiniCssExtractPlugin());
+  
+    return config;
+  }
+  
 };
 
 export default nextConfig;
